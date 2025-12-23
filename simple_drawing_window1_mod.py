1d2e04a7881e76d6621b3a42048e767a261d9fe8
SR67011297 <67011297@kmitl.ac.th>
2025-12-23 17:53:02 +0700

    cool new class

--- simple_drawing_window1_mod.py
+++ simple_drawing_window1_mod.py
@@ -43,11 +43,53 @@ class Simple_drawing_window1(QWidget):
 
         p.end()
 
+class Simple_drawing_window2(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing 2")
+        self.rabbit = QPixmap("images/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 255, 0))
+        p.setBrush(QColor(0, 255, 0))        
+        p.drawRect(0, 0, 1000, 1000)
+
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon(
+            [QPoint(70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 150)]
+        )
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+        p.drawPolygon(
+            [QPoint(50, 200), QPoint(150, 200),
+            QPoint(100, 400)]
+        )
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
+
+        p.setPen(QColor(255, 0, 0))
+        p.setBrush(QColor(255, 0, 0))
+        p.drawRect(210, 400, 300, 30)
+
+        p.setPen(QColor(0, 0, 255))
+        p.setBrush(QColor(0, 0, 255))                
+        p.drawRect(50, 400, 100, 30)
+
+        p.end()
+
 
 def main():
     app = QApplication(sys.argv)
     w = Simple_drawing_window1()
     w.show()
+    v = Simple_drawing_window2()
+    v.show()
     return app.exec()
 
 if __name__ == "__main__":
